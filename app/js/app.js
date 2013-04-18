b813f63e1458b66520d420a3b275e03972fb05aa
Simon Crabtree <simon@crabsticks.net>
2013-04-18 09:34:21 +0100

    Added some more complexity to the controller tests

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,20 @@
 var app = angular.module('app', []);
 
 app.value('model', {
-  title: "a Test App"
+    title: "a Test App",
+    tasks: [
+        {id: '1', description: 'Controller Unit Tests'},
+        {id: '2', description: 'Responsive layout'}
+    ]
 });
 
 app.controller('AppController', function (model, $scope) {
-  $scope.greeting = 'Hello World! this is ' + model.title;
+    $scope.greeting = 'Hello World! this is ' + model.title;
+    $scope.taskCount = model.tasks ? model.tasks.length : 0;
+    $scope.tasks = model.tasks;
+    if ($scope.tasks) {
+        $scope.taskSummaryMessage = 'You have ' + $scope.taskCount + ' task(s) outstanding';
+    } else {
+        $scope.taskSummaryMessage = 'You have nothing to do at the moment, chill out';
+    }
 });
--- test/spec/applicationControllerSpec.js
+++ test/spec/applicationControllerSpec.js
@@ -1,20 +1,50 @@
-var model, scope, controller;
-
-beforeEach(module('app'));
+var model, scope, controller, createController;
 
 describe('App Controller', function () {
-  beforeEach(inject(function ($controller, $rootScope) {
-    model = {
-      title: 'Test Title'
-    };
-    scope = $rootScope.$new();
-    controller = $controller('AppController', {model: model, $scope: scope});
-  }));
-
-  describe('Given the model has a title', function () {
-    it('Should glue the title to the greeting', inject(function ($controller, $rootScope) {
-      expect(controller).toBeTruthy();
-      expect(scope.greeting).toBe('Hello World! this is Test Title');
+    beforeEach(module('app'));
+    beforeEach(inject(function ($controller, $rootScope) {
+        createController = $controller;
+        scope = $rootScope.$new();
     }));
-  });
+
+    describe('Given the model has a title', function () {
+        beforeEach(function () {
+            model = {
+                title: 'Test Title',
+                tasks: [
+                    {id: '1', description: 'Task One'}
+                ]
+            };
+            controller = createController('AppController', {model: model, $scope: scope});
+        });
+
+        it('Should glue the title to the greeting', inject(function ($controller, $rootScope) {
+            expect(controller).toBeTruthy();
+            expect(scope.greeting).toBe('Hello World! this is Test Title');
+        }));
+
+        it('Should display the number of tasks', function () {
+            expect(scope.taskCount).toBe(1);
+        });
+
+        it('Should display the task descriptions', function () {
+            expect(scope.tasks[0].description).toBe('Task One');
+        });
+
+        it('Should display a task summary message', function () {
+            expect(scope.taskSummaryMessage).toBe('You have 1 task(s) outstanding');
+        });
+    });
+
+    describe('Given there are no tasks', function () {
+        beforeEach(function () {
+            model = {
+            }
+            controller = createController('AppController', {model: model, $scope: scope});
+        });
+
+        it('Should display a "No Tasks" message', function () {
+            expect(scope.taskSummaryMessage).toBe('You have nothing to do at the moment, chill out');
+        });
+    });
 });
