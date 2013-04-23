b5b7fc8e181158e1b2b376dfb1b87002c492b72c
Simon Crabtree <simon@crabsticks.net>
2013-04-23 08:26:47 +0100

    Corrected naming error of a test file dependency

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@ files = [
   JASMINE,
   JASMINE_ADAPTER,
   'app/lib/angular.js',
-  'app/lib/angular-ui-states.min.js',
+  'app/lib/angular-ui-states.js',
   'app/lib/ui-bootstrap-tpls-0.2.0.min.js',
   'app/lib/angular-mocks.js',
   'app/js/**/*.js',
